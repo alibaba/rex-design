eb45b19e3cde7d4d6ae9a12b237f61dac1d1ed1b
云思 <james.hsy@alibaba-inc.com>
2021-07-19 21:16:09 +0800

    feat(Range): support marks

--- packages/story/src/range.stories.tsx
+++ packages/story/src/range.stories.tsx
@@ -15,6 +15,54 @@ export function Basic() {
   );
 }
 
+export function WithMarks() {
+  return (
+    <div>
+      <Range
+        style={{ margin: 40 }}
+        defaultValue={50}
+        marks={{
+          0: '0°C',
+          26: '26',
+          37: '37°C',
+          100: '100%',
+        }}
+        onChange={(nextValue) => {
+          console.log('range onChange:', nextValue);
+        }}
+      />
+
+      <Range
+        style={{ margin: 40 }}
+        defaultValue={37}
+        marks={[0, 26, 37, 100]}
+        onChange={(nextValue) => {
+          console.log('range onChange:', nextValue);
+        }}
+      />
+
+      <Range
+        style={{ margin: 40 }}
+        defaultValue={20}
+        marks={20}
+        onChange={(nextValue) => {
+          console.log('range onChange:', nextValue);
+        }}
+      />
+
+      <Range
+        style={{ margin: 40 }}
+        defaultValue={20}
+        marks={20}
+        marksPosition="below"
+        onChange={(nextValue) => {
+          console.log('range onChange:', nextValue);
+        }}
+      />
+    </div>
+  );
+}
+
 export function Disabled() {
   return <Range style={{ margin: 40 }} value={30} disabled />;
 }
