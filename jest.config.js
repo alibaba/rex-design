d2a3611698e470c481efef8501824977929d8962
云思 <james.hsy@alibaba-inc.com>
2022-04-15 15:33:09 +0800

    chore(jest): better monorepo file structure for jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,19 @@
-module.exports = {
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
+/** @type {import("ts-jest/dist/types").InitialOptionsTsJest} */
+const config = {
   preset: 'ts-jest',
-  // testEnvironment: 'node',
-  // testMatch: ['__tests__/*.[jt]s'],
-  // collectCoverageFrom: ['tests/**/*.{ts,tsx,js,jsx}'],
-  transform: { '.(ts|tsx)$': 'ts-jest/dist' },
-  // transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
-  // setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  verbose: true,
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect', './jest.setup.ts'],
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+    '^lodash-es$': 'lodash',
+  },
+  coverageDirectory: 'coverage',
+  testMatch: ['<rootDir>/packages/core/**/?(*.)+(spec|test).[jt]s?(x)'],
 };
+
+module.exports = config;
